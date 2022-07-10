74f3280f703193129c7182488f8640e797d5a59a
Jared <13842260+OofYeetMcGee@users.noreply.github.com>
2022-07-10 06:20:03 -0600

    config stuff

--- config.h
+++ config.h
@@ -2,18 +2,18 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;
+static int centered = 1;
 static int min_width = 500;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Terminus:size=16"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = "run:";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 				  /*     fg         bg       */
-	[SchemeNorm] = { "#000b1e", "#000b1e" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#91ffff", "#000b1e" },
+	[SchemeSel] = { "#333333", "#91ffff" },
+	[SchemeOut] = { "#bbbbbb", "#000b1e" },
 	[SchemeHp] = { "#c6c6c6", "#00ddf4" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
